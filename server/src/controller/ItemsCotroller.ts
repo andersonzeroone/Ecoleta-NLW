02693918317e81cf4d5bc7daf128d6869bc4be0a
andersonzeroone <anderson.pablo02@gmail.com>
2020-06-07 19:11:25 -0300

    add map

--- server/src/controller/ItemsCotroller.ts
+++ server/src/controller/ItemsCotroller.ts
@@ -11,7 +11,7 @@ class ItemsController{
                
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`
+                image_url: `http://192.168.0.118:3333/uploads/${item.image}`
             }
         })
     
